af148ff15e98bc0195ccd15a3ede5512fa3d4ebf
jpinn97 <JP878@canterbury.ac.uk>
2023-04-19 01:09:02 +0100

    test

--- source.py
+++ source.py
@@ -1,17 +1,58 @@
-import subprocess
-
-# Install the arff library using pip
-subprocess.check_call(['pip', 'install', 'liac-arff', '--quiet'])
-
+import numpy as np
+import pandas as pd
 import arff
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
-with open('phpgNaXZe.arff') as f:
-    dataset = arff.load(f, encode_nominal=True)
+# Load the dataset
+with open("phpgNaXZe.arff", "r") as f:
+    dataset = arff.load(f)
+data = np.array(dataset["data"])
+df = pd.DataFrame(data, columns=[attr[0] for attr in dataset["attributes"]])
 
-# Get the attribute names and data from the dataset
-attributes = [attr[0] for attr in dataset['attributes']]
-data = dataset['data']
+# Preprocessing
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values.astype(int) - 1  # Convert labels to integers and subtract 1 to have labels in {0, 1}
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-# Print the attribute names and data
-print(attributes)
-print(data)
+# Create a neural network model
+model = Sequential()
+model.add(Dense(16, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=5000, batch_size=64, verbose=1)
+
+# Evaluate the model
+_, accuracy = model.evaluate(X_test, y_test, verbose=1)
+print('Accuracy: %.2f' % (accuracy * 100))
+
+# Evaluate against test data
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
+
+# Make prediction
+predictions = model.predict(X_test)
+
+# Convert predictions to bianry
+binary_predictions = (predictions > 0.5).astype(int)
+
+# Analyze performance
+from sklearn.metrics import classification_report, confusion_matrix
+
+print("Classification Report:")
+print(classification_report(y_test, binary_predictions))
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, binary_predictions))
+

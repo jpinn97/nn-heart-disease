11de47abbd1970c67656872ac1c97a55b6b73aab
jpinn97 <JP878@canterbury.ac.uk>
2023-04-21 21:57:34 +0100

    auto-sklearn

--- source_best.py
+++ source_best.py
@@ -1,11 +1,52 @@
+import shutil
+shutil.rmtree("/tmp/autosklearn_classification_example_tmp", ignore_errors=True)
+
+import autosklearn.classification
+from autosklearn.pipeline.components.classification import _classifiers
+from ConfigSpace import ConfigurationSpace
+from ConfigSpace.hyperparameters import CategoricalHyperparameter
 import numpy as np
 import pandas as pd
 import arff
-import matplotlib.pyplot as plt
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
-from autosklearn.classification import AutoSklearnClassifier
+from pprint import pprint
+
+
+# Custom AutoSklearnClassifier class
+class CustomAutoSklearnClassifier(autosklearn.classification.AutoSklearnClassifier):
+    def _get_hyperparameter_search_space(self, dataset_properties=None):
+        cs = ConfigurationSpace()
+
+        # Restrict the configuration space to MLP
+        allowed_classifiers = ["mlp"]
+        classifier_cs = restrict_classifiers(_classifiers, allowed_classifiers)
+        cs.add_configuration_space("classifier", classifier_cs)
+
+        return cs
+
+
+# Custom function to restrict classifier types
+def restrict_classifiers(
+    classifiers: dict, allowed_classifiers: list
+) -> ConfigurationSpace:
+    restricted_classifiers = {
+        key: value for key, value in classifiers.items() if key in allowed_classifiers
+    }
+    cs = ConfigurationSpace()
+    cs.add_hyperparameter(
+        CategoricalHyperparameter(
+            "__choice__", choices=list(restricted_classifiers.keys())
+        )
+    )
+    for component in restricted_classifiers.values():
+        cs.add_configuration_space(
+            component.__name__, component.get_hyperparameter_search_space()
+        )
+    return cs
+
 
 # Load the dataset
 with open("phpgNaXZe.arff", "r") as f:
@@ -19,21 +60,24 @@ y = df.iloc[:, -1].values.astype(int) - 1
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+    X, y, random_state=1
 )
 
-# Create an AutoML classifier with only neural networks
-automl = AutoSklearnClassifier(
-    time_left_for_this_task=600,
-    ensemble_size=0,
-    include_estimators=["mlp", "adaboost-mlp"],
-    exclude_estimators=["libsvm_svc", "sgd", "pa", "passive_aggressive"],
+automl = CustomAutoSklearnClassifier(
+    time_left_for_this_task=1800,
+    per_run_time_limit=60,
+    tmp_folder="/tmp/autosklearn_classification_example_tmp",
 )
 
-# Fit the AutoML classifier to the training data
 automl.fit(X_train, y_train)
+
+print(automl.leaderboard())
+
+pprint(automl.show_models(), indent=4)
+
+predictions = automl.predict(X_test)
+print("Accuracy score:", sklearn.metrics.accuracy_score(y_test, predictions))
 
 # Evaluate the AutoML classifier on the test data
 y_pred = automl.predict(X_test)
